991fa4c131acd9207964ee2d2359ebc04d820911
Ali <73517844+Ali-00@users.noreply.github.com>
2021-01-29 22:16:26 +0500

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -31,7 +31,7 @@ def search():
     weather = get_weather(city)
     if weather:
         location_lbl['text'] = '{}, {}'.format(weather[0],weather[1])
-        # image['bitmap'] = 'weather_icons/{}.png'.format(weather[4])
+    
         temp_lbl['text'] = '{:2f}°C, {:2f}°F'.format(weather[2],weather[3])
         weather_lbl['text'] = weather[5]
     else:
@@ -51,8 +51,6 @@ search_btn.pack()
 location_lbl = Label(app, text='', font=('bold',20))
 location_lbl.pack()
 
-# image = Label(app, bitmap='')
-# image.pack()
 
 temp_lbl = Label(app, text='')
 temp_lbl.pack()
